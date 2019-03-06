91fca5e04421a9dbff4804642d80be83c02a0122
Jake <jzb0150@auburn.edu>
2019-03-06 00:36:49 -0600

    Finished part 2 entirely

---
+++ part_2/aug_position_with_help_BOTELLO.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+while getopts ":h" opt;
+do
+    case $opt in
+        h)
+            echo "You're getting help!  Give three arguments in succession (e.g. filename arg1 arg2 arg3) and you'll see them echoed in reverse-order." >&2
+            ;;
+        :)
+            echo "You should give three arguments instead of the thing you're doing!"
+    esac
+done
+
+echo $3
+echo $2
+echo $1
+
+
